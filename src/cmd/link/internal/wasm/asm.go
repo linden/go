b460aa1fb3a7f75a27037b8dbf7193ac49f65153
Linden <70739041+linden@users.noreply.github.com>
2025-03-17 13:58:10 -0700

    wip-chore: remove unused `wasm_export_resume`

--- src/cmd/link/internal/wasm/asm.go
+++ src/cmd/link/internal/wasm/asm.go
@@ -421,7 +421,7 @@ func writeExportSec(ctxt *ld.Link, ldr *loader.Loader, lenHostImports int) {
 
 	switch buildcfg.GOOS {
 	case "wasip1":
-		writeUleb128(ctxt.Out, uint64(4+len(ldr.WasmExports))) // number of exports
+		writeUleb128(ctxt.Out, uint64(3+len(ldr.WasmExports))) // number of exports
 		var entry, entryExpName string
 		switch ctxt.BuildMode {
 		case ld.BuildModeExe:
@@ -437,14 +437,6 @@ func writeExportSec(ctxt *ld.Link, ldr *loader.Loader, lenHostImports int) {
 		}
 		idx := uint32(lenHostImports) + uint32(ldr.SymValue(s)>>16) - funcValueOffset
 		writeName(ctxt.Out, entryExpName)   // the wasi entrypoint
-		ctxt.Out.WriteByte(0x00)            // func export
-		writeUleb128(ctxt.Out, uint64(idx)) // funcidx
-		s = ldr.Lookup("wasm_export_resume", 0)
-		if s == 0 {
-			ld.Errorf("export symbol %s not defined", entry)
-		}
-		idx = uint32(lenHostImports) + uint32(ldr.SymValue(s)>>16) - funcValueOffset
-		writeName(ctxt.Out, "resume")       // the wasi entrypoint
 		ctxt.Out.WriteByte(0x00)            // func export
 		writeUleb128(ctxt.Out, uint64(idx)) // funcidx
 		s = ldr.Lookup("wasm_export_getsuspend", 0)
--- src/runtime/rt0_wasip1_wasm.s
+++ src/runtime/rt0_wasip1_wasm.s
@@ -6,8 +6,6 @@
 #include "textflag.h"
 
 TEXT _rt0_wasm_wasip1(SB),NOSPLIT,$0
-	I32Const $wasm_export_resume(SB)
-	Drop
 	I32Const $wasm_export_getsuspend(SB)
 	Drop
 
@@ -22,11 +20,6 @@ TEXT _rt0_wasm_wasip1(SB),NOSPLIT,$0
 
 TEXT _rt0_wasm_wasip1_lib(SB),NOSPLIT,$0
 	Call _rt0_wasm_wasip1(SB)
-	Return
-
-TEXT wasm_export_resume(SB),NOSPLIT,$0
-	Call wasm_pc_f_loop(SB)
-
 	Return
 
 TEXT wasm_export_getsuspend(SB),NOSPLIT,$0
